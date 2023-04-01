d06fdfc1ba10989f22712f8a22cd8f63c884aae3
dbennes <dbennes97@gmail.com>
2023-04-01 14:51:59 -0300

    test

---
+++ seed-walkers/src/pages/googleFit.js
@@ -0,0 +1,50 @@
+import { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+
+const SCOPES = ['https://www.googleapis.com/auth/fitness.activity.read'];
+const CLIENT_ID = 'YOUR_CLIENT_ID'; // insira seu ID de cliente do Google
+
+function authorizeUser() {
+  const authClient = new OAuth2Client(CLIENT_ID);
+
+  authClient.authorize({ scope: SCOPES, prompt: 'consent' }, (err, token) => {
+    if (err) {
+      console.log('Erro de autorização:', err);
+      return;
+    }
+    console.log('Autorizado com sucesso!');
+    getDataFromGoogleFit(token);
+  });
+}
+
+function getDataFromGoogleFit(auth) {
+  const fitness = google.fitness('v1');
+  fitness.users.dataset.aggregate({
+    auth: auth,
+    userId: 'me',
+    requestBody: {
+      "aggregateBy": [{ "dataTypeName": "com.google.step_count.delta", "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps" }],
+      "bucketByTime": { "durationMillis": 86400000 },
+      "startTimeMillis": new Date(new Date().setDate(new Date().getDate() - 7)).getTime(),
+      "endTimeMillis": new Date().getTime()
+    }
+  }, (err, res) => {
+    if (err) {
+      console.log('Erro ao obter dados:', err);
+      return;
+    }
+    console.log('Dados recebidos com sucesso:', res.data);
+    renderData(res.data);
+  });
+}
+
+function renderData(data) {
+  return (
+    <div>
+      <h1>Dados de fitness do Google Fit:</h1>
+      {/* renderize os dados na sua interface do usuário */}
+    </div>
+  );
+}
+
+export { authorizeUser, getDataFromGoogleFit, renderData };
--- seed-walkers/src/pages/index.js
+++ seed-walkers/src/pages/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { authorizeUser, getDataFromGoogleFit, renderData } from './googleFit';
 
-const index = () => {
+function App() {
+  const [data, setData] = useState(null);
+
+  function handleClick() {
+    authorizeUser();
+  }
+
+  function renderGoogleFitData() {
+    if (data) {
+      return renderData(data);
+    } else {
+      return <p>Clique no botão para autorizar e obter os dados do Google Fit.</p>;
+    }
+  }
+
   return (
-    <div>index</div>
-  )
+    <div>
+      <button onClick={handleClick}>Autorizar e obter dados do Google Fit</button>
+      {renderGoogleFitData()}
+    </div>
+  );
 }
 
-export default index
+export default App;
